f4265b27f9914b3f436556709b7c22120d2a81a8
ravisingh-0 <ravi.singh@soekalkaji.co.in>
2024-05-20 01:16:17 +0530

    Lecture 7 in Class

---
+++ 18-05-2024/PrimeNumber1_n.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class PrimeNumber1_n{
+    public static void main(String args[]){
+        int count;
+        for(int i=1; i<=100; i++){
+            count = 0;
+            for(int j=1; j<=i; j++){
+                if(i%j==0){
+                    count++;
+                }
+                
+            }
+            if(count == 2 && !(count>2)){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
